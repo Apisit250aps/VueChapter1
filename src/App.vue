aa4250e6cc022dc7ed80422563699248c81add29
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-11-12 11:45:08 +0700

    set template

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
+import { RouterLink, RouterView } from "vue-router"
+import SideMenu from "./components/navigates/SideMenu.vue"
+import NavbarMenu from "./components/navigates/NavbarMenu.vue"
 </script>
 
 <template>
-  <RouterView />
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+      <SideMenu />
+      <div class="layout-page">
+        <NavbarMenu />
+        <div class="content-wrapper">
+          <div class="container-xxl flex-grow-1 container-p-y">
+            <RouterView />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
+<style scoped></style>
